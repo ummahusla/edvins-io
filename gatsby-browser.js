42cf142d29a5cb1d6aeea487dcfffc01bcddb0be
Edvins Antonovs <ummahusla@gmail.com>
2021-02-10 16:30:11 +0200

    Fixed issue with SW

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,5 @@
 import './styles.css';
+
+// SW doesn't refresh the website
+// https://github.com/gatsbyjs/gatsby/issues/9087
+export const onServiceWorkerUpdateReady = () => window.location.reload();
