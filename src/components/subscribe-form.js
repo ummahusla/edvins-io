3df3d6cdd806ef3b5cc7f5fdc7e6c93941eda076
Edvins Antonovs <ummahusla@gmail.com>
2023-01-05 14:16:23 +0200

    feat: Replaced revue with substack

--- src/components/subscribe-form.js
+++ src/components/subscribe-form.js
@@ -4,11 +4,12 @@ import Title from '@lekoarts/gatsby-theme-minimal-blog/src/components/title';
 
 const SubscribeForm = () => (
   <div
+    className="newsletter-form"
     sx={{
       mt: 5,
     }}
   >
-    <Title text="Subscribe to the newsletter" />
+    <Title text="Newsletter" />
 
     <p
       sx={{
@@ -17,84 +18,21 @@ const SubscribeForm = () => (
         fontSize: [1, 1, 2],
       }}
     >
-      Get emails from me about web development, tech and entrepreneurship.
+      Sign up to get updates when I write something new. No spam ever.
     </p>
 
-    <div id="revue-embed">
-      <form
-        className="revue-embed-form"
-        action="https://www.getrevue.co/profile/edvins/add_subscriber"
-        method="post"
-        id="revue-form"
-        name="revue-form"
-        target="_blank"
-      >
-        <div className="revue-form-group">
-          <label
-            sx={{
-              color: `text`,
-              mt: 1,
-              fontSize: [1, 1, 2],
-            }}
-            htmlFor="member_first_name"
-          >
-            First name
-          </label>
-
-          <input
-            className="revue-form-field"
-            placeholder="Elon"
-            type="text"
-            name="member[first_name]"
-            id="member_first_name"
-            sx={{
-              color: `secondary`,
-              fontSize: [1],
-            }}
-          />
-        </div>
-
-        <div className="revue-form-group">
-          <label
-            sx={{
-              color: `text`,
-              mt: 1,
-              fontSize: [1, 1, 2],
-            }}
-            htmlFor="member_email"
-          >
-            Email
-          </label>
-
-          <input
-            className="revue-form-field"
-            placeholder="elon@tesla.com"
-            type="email"
-            name="member[email]"
-            id="member_email"
-            sx={{
-              color: `secondary`,
-              fontSize: [1],
-            }}
-          />
-        </div>
-
-        <div className="revue-form-actions">
-          <input
-            type="submit"
-            value="Subscribe"
-            className="revue-form-submit"
-            name="member[subscribe]"
-            id="member_submit"
-            sx={{
-              color: `heading`,
-              mt: 1,
-              fontSize: [1],
-            }}
-          />
-        </div>
-      </form>
-    </div>
+    <a
+      href="https://edvins.substack.com/"
+      target="_blank"
+      className="newsletter-form-submit"
+      sx={{
+        color: `heading`,
+        mt: 1,
+        fontSize: [1, 1, 2],
+      }}
+    >
+      Subscribe to my Newsletter
+    </a>
   </div>
 );
 
