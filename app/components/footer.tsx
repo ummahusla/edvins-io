ed81b359c03a46c2de7e819d2efdd8c8b8973831
Edvins Antonovs <ummahusla@gmail.com>
2025-10-21 21:36:22 +0300

    feat: easter egg

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -51,6 +51,17 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
+            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">onlyfans</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
             href="https://github.com/ummahusla"
           >
             <ArrowIcon />
